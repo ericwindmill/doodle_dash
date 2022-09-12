762c95c1cd26f3c785f03aef6f4e393742fae0cf
Eric Windmill <ewindmill@google.com>
2022-09-12 11:44:25 -0700

    removes flutter create boiler plate and adds keyboard handler

--- lib/game/sprites/player.dart
+++ lib/game/sprites/player.dart
@@ -1,17 +1,47 @@
 import 'package:flame/components.dart';
+import 'package:flutter/services.dart';
 
 import '../doodle_dash.dart';
 
-class Player extends SpriteComponent with HasGameRef<DoodleDash> {
+enum DashDirection { left, right }
+
+class Player extends SpriteGroupComponent<DashDirection>
+    with HasGameRef<DoodleDash>, KeyboardHandler {
   Player() : super(size: Vector2.all(100));
   int count = 0;
 
   @override
   Future<void> onLoad() async {
+    print('Player.onLoad');
     super.onLoad();
-    sprite = await gameRef.loadSprite('game/right_dash.png');
+    final leftDash = await gameRef.loadSprite('game/left_dash.png');
+    final rightDash = await gameRef.loadSprite('game/right_dash.png');
+
+    sprites = <DashDirection, Sprite>{
+      DashDirection.left: leftDash,
+      DashDirection.right: rightDash,
+    };
+
+    // arbitrarily start with Dash facing right
+    current = DashDirection.right;
   }
 
   @override
   void update(double dt) {}
+
+  @override
+  bool onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    print('Player.onKeyPressed');
+    if (keysPressed.contains(LogicalKeyboardKey.arrowLeft)) {
+      print('KEY LEFT!');
+      current = DashDirection.left;
+    }
+
+    if (keysPressed.contains(LogicalKeyboardKey.arrowRight)) {
+      print('KEY LEFT!');
+      current = DashDirection.right;
+    }
+
+    return true;
+  }
 }
