e7b1b2468b87b4206de6b9e47468e8bdf7f3c7f7
Eric Windmill <ewindmill@google.com>
2022-11-09 15:03:23 -0800

    check in

--- lib/game/world.dart
+++ lib/game/world.dart
@@ -7,7 +7,14 @@ class World extends ParallaxComponent<DoodleDash> {
   @override
   Future<void> onLoad() async {
     parallax = await gameRef.loadParallax(
-      [ParallaxImageData('game/test-space-bg.png')],
+      [
+        ParallaxImageData('game/06_Background_Solid.png'),
+        // ParallaxImageData('game/background/01_Background_Squiggles.png'),
+        // ParallaxImageData('game/background/02_Background_Orbs.png'),
+        // ParallaxImageData('game/background/03_Background_Block_Shapes.png'),
+        // ParallaxImageData('game/background/04_Background_Big_Stars.png'),
+        // ParallaxImageData('game/background/05_Background_Small_Stars.png'),
+      ],
     );
   }
 }
